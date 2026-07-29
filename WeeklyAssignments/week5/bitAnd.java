cb16715b7a1b876f7cf909abd19668a11f7e55b1
Ketan1317 <goyalketan1317@gmail.com>
2026-07-29 23:47:25 +0530

    implement bitAnd

--- WeeklyAssignments/week5/bitAnd.java
+++ WeeklyAssignments/week5/bitAnd.java
@@ -3,9 +3,12 @@ public class bitAnd {
         int a = 6;
         int b = 5;
         System.out.println("Answer with & operator: " + (a&b));
-        // ~(a&b) = ~a | ~b
-        // ~(~(a&b)) = ~(~a | ~b)
-        int ans = ~(~a | ~b);
-        System.out.println("Answer without & operator: " + ans);
+        
+        System.out.println("Answer without & operator: " + bitAndOp(a, b));
+    }
+    public static int bitAndOp(int x, int y) {
+        // ~(x&y) = ~x | ~y
+        // ~(~(x&y)) = ~(~x | ~y)
+        return ~(~x | ~y);
     }
 }
